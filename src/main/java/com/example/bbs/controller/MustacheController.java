ea8b68967ef86b73d5c1135491ab93a517273192
dmdwns2 <ssreki1@naver.com>
2022-11-07 11:49:54 +0900

    [mustache] SpringBoot-Mustache
    1. greetings.mustache 파일 생성
    2. MustacheController 클래스 및 메서드 생성

--- src/main/java/com/example/bbs/controller/MustacheController.java
+++ src/main/java/com/example/bbs/controller/MustacheController.java
@@ -1,9 +1,12 @@
 package com.example.bbs.controller;
 
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@Controller
 public class MustacheController {
 
     @GetMapping(value = "/hi")
