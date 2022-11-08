7d15b742915386a17ba0b0e7d7b52d8e74482d1e
dmdwns2 <ssreki1@naver.com>
2022-11-08 15:35:25 +0900

    [mustache] CRUD 연습
    list, error ,show mustache 추가
    컨트롤러에 기능추가
    오류 해결

--- src/main/java/com/example/bbs/controller/ArticleController.java
+++ src/main/java/com/example/bbs/controller/ArticleController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -35,17 +36,30 @@ public class ArticleController {
 
         if(!optionalArticle.isEmpty()){
             model.addAttribute("article", optionalArticle.get());
-            return "show";
+            return "articles/show";
         } else {
-            return "error";
+            return "articles/error";
         }
     }
-    @PostMapping("/post")
-    public String articles(ArticleDto articleDto) {
-        log.info(articleDto.toString());
-        articleRepository.save(articleDto.toEntity());
-        return "";
+
+//    @GetMapping("/list")
+//    public String list(){
+//        return "articles/list";
+//    }
+
+    @GetMapping("/list")
+    public String list(Model model) {
+        List<Article> articles = articleRepository.findAll();
+        model.addAttribute("articles", articles);
+        return "articles/list";
+
     }
+
+    @GetMapping("")
+    public String index(){
+        return "redirect:/articles/list";
+    }
+
 
     @PostMapping("")
     public String add(ArticleDto articleDto) {
